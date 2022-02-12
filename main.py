6ad1a0087e84db2dd718e122a06388738f11477f
shivam <shivam23sherkar@gmail.com>
2022-02-12 13:32:27 +0530

    adduser functionality

--- main.py
+++ main.py
@@ -1,39 +1,37 @@
-from fastapi import FastAPI, Request, status, HTTPException
+from fastapi import Body, FastAPI, status, HTTPException
 import psycopg2
 from psycopg2.extras import DictCursor
-from typing import Optional
-from fastapi.params import Body
-from pydantic import BaseModel    # to define the schema of our data
-from random import randrange
+from datetime import datetime
 
-from starlette.responses import Response
 app = FastAPI()
 
 try:
-    con = psycopg2.connect(host='localhost',database='postgres',user='postgres',password='admin123')
+    con = psycopg2.connect(
+        host='localhost', database='postgres', user='postgres', password='admin123')
     cursor = con.cursor(cursor_factory=DictCursor)
     print("database connection successful")
 except Exception as ex:
     print(ex)
 
+
 def get_table_data(cursor):
-  cursor.execute('select * from accounts;')
-  result = cursor.fetchall()
-  output = []
-  for x in result:
-    op_dict = {
-      "id":x[0],
-      "username":x[1],
-      "password":x[2],
-      "email":x[3],
-      "created_on":x[4],
-      "last_login":x[5],
-    }
-    output.append(op_dict)
+    cursor.execute('select * from accounts;')
+    result = cursor.fetchall()
+    output = []
+    for x in result:
+        op_dict = {
+            "id": x[0],
+            "username": x[1],
+            "password": x[2],
+            "email": x[3],
+            "created_on": x[4],
+            "last_login": x[5],
+        }
+        output.append(op_dict)
 #   cursor.close()
 #   con.close()
-  con.commit()
-  return output
+    con.commit()
+    return output
 
 
 @app.get("/")
@@ -41,14 +39,16 @@ def read_root():
     accounts_data = get_table_data(cursor)
     return accounts_data
 
-@app.post("/check_users", status_code=status.HTTP_201_CREATED)
-def put_posts(id):
-    print(id)
-    cursor.execute(f'select * from accounts where id = {id};')
-    result = cursor.fetchall()
-    cursor.close()
-    con.commit()
-    return result
+# def put_posts(id,name,password)
+@app.put("/add_accounts", status_code=status.HTTP_201_CREATED)
+def put_posts(account: str = Body(..., embed=True)):
+    # query = f"""insert into accounts values({id},'{name}','{password}',"{name}@mail.com",datetime.now(),datetime.now());"""
+    # cursor.execute(query)
+    # result = cursor.fetchall()
+    # cursor.close()
+    # con.commit()
+    return account
+
 
 @app.get("/accounts/{id}")
 def get_accounts(id: int):
